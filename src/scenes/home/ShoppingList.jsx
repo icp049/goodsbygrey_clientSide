0b971771b8cd890cdba078230698ca87211f9c3a
icp049 <ianpepe2000@gmail.com>
2023-06-30 19:57:37 -0600

    added shopping list

---
+++ src/scenes/home/ShoppingList.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react';
+import {Box, Typography, Tab, Tabs, useMediaQuery} from '@mui/material';
+import Item from '../../components';
+
+
+
+
+
+const ShoppingList = () => {
+
+}
+
+export default ShoppingList
