6e40b32785695e5ac6e978017f28c625e6d18925
icp049 <ianpepe2000@gmail.com>
2023-07-11 20:57:54 -0600

    prototyping the confirmation message

--- src/scenes/checkout/Confirmation.jsx
+++ src/scenes/checkout/Confirmation.jsx
@@ -1,5 +1,15 @@
+import { Box, Alert, AlertTitle, Typography } from '@mui/material';
+
 const Confirmation = () => {
-    return <div>Home </div>
+  return (
+    <Box m="90px auto" width="80%" height="50vh">
+      <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
+        You have successfully made an order -{' '}
+        <Typography component="strong">Congratulations on your purchase</Typography>
+      </Alert>
+    </Box>
+  );
 };
 
 export default Confirmation;
