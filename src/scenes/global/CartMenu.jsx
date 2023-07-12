ea4cb413581cf2c66dbd7dd4b9cd01fd6dd6d2c0
icp049 <ianpepe2000@gmail.com>
2023-07-12 01:19:26 -0600

    added cartrestriction when empty and warning

--- src/scenes/global/CartMenu.jsx
+++ src/scenes/global/CartMenu.jsx
@@ -1,4 +1,5 @@
-import {Box, Button, Divider, IconButton, Typography} from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import CloseIcon from "@mui/icons-material/Close";
 import AddIcon from "@mui/icons-material/Add";
@@ -6,19 +7,18 @@ import RemoveIcon from "@mui/icons-material/Remove";
 import styled from "@emotion/styled";
 
 import {
-decreaseCount,
-increaseCount,
-removeFromCart,
-setIsCartOpen,
+  decreaseCount,
+  increaseCount,
+  removeFromCart,
+  setIsCartOpen,
 } from "../../state";
 
-import {useNavigate} from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 const FlexBox = styled(Box)`
- display: flex;
- justify-content: spacebetween;
- align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const CartMenu = () => {
@@ -26,141 +26,138 @@ const CartMenu = () => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.cart);
   const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+  const [errorMessage, setErrorMessage] = useState(""); // Move it inside the component function
 
   const totalPrice = cart.reduce((total, item) => {
     return total + item.count * item.attributes.price;
   }, 0);
 
+  useEffect(() => {
+    if (!isCartOpen) {
+      setErrorMessage(""); // Reset the error message when isCartOpen becomes false
+    }
+  }, [isCartOpen]);
+
   return (
     <Box
-    display = {isCartOpen ? "block" : "none"}
-    backgroundColor = "rgba(0, 0, 0, 0.9)"
-    position = "fixed"
-    zIndex = {10}
-    width = "100%"
-    height = "100%"
-    left = "0"
-    top = "0"
-    overflow = "auto"
-    >    
-
-        
-        <Box
-        position = "fixed"
-        right = "0"
-        bottom = "0"
-        width = "max(400px, 30%)"
-        height = "100%"
-        backgroundColor = "white"
-        >
-
-        <Box padding = "30px" overflow = "auto" height = "100%"
-        >{}
-         <FlexBox mb = "15px">
-            <Typography variant = "h5"> SHOPPING BAG ({cart.length})</Typography>
-            <IconButton onClick = {() => dispatch(setIsCartOpen({}))}>
-                <CloseIcon/>
+      display={isCartOpen ? "block" : "none"}
+      backgroundColor="rgba(0, 0, 0, 0.9)"
+      position="fixed"
+      zIndex={10}
+      width="100%"
+      height="100%"
+      left="0"
+      top="0"
+      overflow="auto"
+    >
+      <Box
+        position="fixed"
+        right="0"
+        bottom="0"
+        width="max(400px, 30%)"
+        height="100%"
+        backgroundColor="white"
+      >
+        <Box padding="30px" overflow="auto" height="100%">
+          <FlexBox mb="15px">
+            <Typography variant="h5"> SHOPPING BAG ({cart.length})</Typography>
+            <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+              <CloseIcon />
             </IconButton>
-         </FlexBox>
+          </FlexBox>
 
-          {}
-         <Box>
+          <Box>
             {cart.map((item) => (
-                <Box key = {`${item.attributes.name} - ${item.id}`}>
-                     <FlexBox p = "15px 0">
-                        
-                        <Box flex="1 1 40%">
-  <img
-    alt={item?.name}
-    width="123px"
-    height="154px"
-    src={`http://localhost:1337${item?.attributes.image?.data?.attributes?.formats?.medium?.url}`}
-  />
-</Box>
-                     <Box flex = "1 1 60%">
-                        <FlexBox mb = "5px">
-                           <Typography fontWeight = "bold">
-                            {item.attributes.name}
-                           </Typography>
-                           <IconButton onClick  ={()=> dispatch(removeFromCart({id: item.id}))}>
-                            <CloseIcon/>
+              <Box key={`${item.attributes.name} - ${item.id}`}>
+                <FlexBox p="15px 0">
+                  <Box flex="1 1 40%">
+                    <img
+                      alt={item?.name}
+                      width="123px"
+                      height="154px"
+                      src={`http://localhost:1337${item?.attributes.image?.data?.attributes?.formats?.medium?.url}`}
+                    />
+                  </Box>
+                  <Box flex="1 1 60%">
+                    <FlexBox mb="5px">
+                      <Typography fontWeight="bold">
+                        {item.attributes.name}
+                      </Typography>
+                      <IconButton
+                        onClick={() => dispatch(removeFromCart({ id: item.id }))}
+                      >
+                        <CloseIcon />
+                      </IconButton>
+                    </FlexBox>
+                    <Typography>{item.attributes.shortDescription}</Typography>
+                    <FlexBox m="15px 0">
+                      <Box
+                        mt="5px"
+                        display="flex"
+                        alignItems="center"
+                        border={`1.5px solid blue`}
+                      >
+                        <IconButton
+                          onClick={() => dispatch(decreaseCount({ id: item.id }))}
+                        >
+                          <RemoveIcon />
+                        </IconButton>
 
-                            </IconButton>
-   
-                        </FlexBox>
-                          <Typography> {item.attributes.shortDescription}</Typography>
-                          <FlexBox m = "15px 0">
-                            <Box
-                            mt  ="5px"
-                            display = "flex"
-                            alingnItems = "center"
-                            border = {`1.5px solid blue`}
-                            >
-                                 <IconButton
-                                    onClick = {() => dispatch(decreaseCount({id: item.id}))}
-                                    >
-                                        <RemoveIcon />
-                                 </IconButton>
+                        <Typography mt="8px">{item.count}</Typography>
 
-                                 <Typography mt = "8px">{item.count}</Typography>
-
-
-                                 <IconButton
-                                    onClick = {() => dispatch(increaseCount({id: item.id}))}
-                                    >
-                                        <AddIcon />
-                                 </IconButton>
-
-                                 
-
-                            </Box>
-
-                           
-
-                          </FlexBox>
-
-                          
-                     </Box>
-                        
-                        </FlexBox>   
-                        <Divider />                      
-                </Box>
-            ))}
-         </Box>
-              
-             {/*actions*/}
-
-             <Box m = "20px 0">
-                <FlexBox m = "20px 0">
-                    <Typography fontWeight = "bold"> SUBTOTAL:</Typography>
-                    <Typography fontWeight = "bold" ml = "5px">  ${totalPrice}</Typography>
+                        <IconButton
+                          onClick={() => dispatch(increaseCount({ id: item.id }))}
+                        >
+                          <AddIcon />
+                        </IconButton>
+                      </Box>
+                    </FlexBox>
+                  </Box>
                 </FlexBox>
-                <Button sx = {{ backgroundColor: "blue", color: "white", borderRadius: 0, minWidth: "100%", 
-            padding: "20px 40px", m:"20px 0"}}
-            onClick = {() => {
-                navigate("/checkout");
-                dispatch(setIsCartOpen({}));
-            } }
+                <Divider />
+              </Box>
+            ))}
+          </Box>
+
+          {errorMessage && (
+            <Typography variant="body1" color="error" mb={2}>
+              {errorMessage}
+            </Typography>
+          )}
+
+          <Box m="20px 0">
+            <FlexBox m="20px 0">
+              <Typography fontWeight="bold"> SUBTOTAL:</Typography>
+              <Typography fontWeight="bold" ml="5px">
+                ${totalPrice}
+              </Typography>
+            </FlexBox>
+            <Button
+              sx={{
+                backgroundColor: "blue",
+                color: "white",
+                borderRadius: 0,
+                minWidth: "100%",
+                padding: "20px 40px",
+                m: "20px 0",
+              }}
+              onClick={() => {
+                if (cart.length > 0) {
+                  navigate("/checkout");
+                  dispatch(setIsCartOpen({}));
+                  setErrorMessage(""); 
+                } else {
+                  setErrorMessage("Cart is empty");
+                }
+              }}
             >
-                    CHECKOUT
-                </Button>
-             </Box>
-
-
-
-
-
-
+              CHECKOUT
+            </Button>
+          </Box>
         </Box>
-
-                  
-
-
-
-        </Box>
-
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default CartMenu;
