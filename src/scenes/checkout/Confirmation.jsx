12484133c7f50f20e77d0e4b6f4f2a25cbe9edd5
icp049 <ianpepe2000@gmail.com>
2023-07-18 01:34:30 -0600

    changed something in confirmation to reflect order number

--- src/scenes/checkout/Confirmation.jsx
+++ src/scenes/checkout/Confirmation.jsx
@@ -1,13 +1,30 @@
+// Confirmation.js
+import React, { useEffect, useState } from 'react';
 import { Box, Alert, AlertTitle, Typography } from '@mui/material';
 
-const Confirmation = () => {
+const Confirmation = ({ orderId }) => {
+  const [orderNumber, setOrderNumber] = useState('');
+
+  useEffect(() => {
+    // Fetch the order details from the server using the orderId prop
+    fetch(`/api/orders/${orderId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Assuming the server response contains the order number as "id"
+        setOrderNumber(data.id.toString());
+      })
+      .catch((error) => {
+        console.error('Error fetching order number:', error);
+      });
+  }, [orderId]);
+
   return (
-    <Box m="90px auto" width="80%" height="50vh" bgcolor="rgba(0, 128, 0, 0.1)" // Transparent green box background
-    borderRadius="10px">
+    <Box m="90px auto" width="80%" height="50vh">
       <Alert severity="success">
         <AlertTitle>Success</AlertTitle>
         You have successfully made an order -{' '}
         <Typography component="strong">Congratulations on your purchase</Typography>
+        <Typography>Order Number: {orderNumber}</Typography>
       </Alert>
     </Box>
   );
